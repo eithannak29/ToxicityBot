a1a1f0655e5818595e9420b34b40397762765b35
Camil Ziane <camilbel@live.fr>
2024-05-09 14:54:48 +0200

    feat(latex)

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,5 @@
-TEST_LABEL_PATH = '../jigsaw-toxic-comment-classification-challenge/test_labels.csv'
-TEST_PATH = '../jigsaw-toxic-comment-classification-challenge/test.csv'
-TRAIN_PATH = '../jigsaw-toxic-comment-classification-challenge/train.csv'
+TEST_LABEL_PATH = 'jigsaw-toxic-comment-classification-challenge/test_labels.csv'
+TEST_PATH = 'jigsaw-toxic-comment-classification-challenge/test.csv'
+TRAIN_PATH = 'jigsaw-toxic-comment-classification-challenge/train.csv'
 
 CATEGORIES = ["toxic","severe_toxic","obscene", "threat", "insult", "identity_hate"]
