f69bd47c9019a3e01dfe2f21a1582dd3983bcf3f
Camil Ziane <camilbel@live.fr>
2024-05-16 16:29:12 +0200

    feat(binary): tf-idf word2vec donc

--- src/utils/tokenize_api.py
+++ src/utils/tokenize_api.py
@@ -1,4 +1,3 @@
-import sys
 import nltk
 from nltk import pos_tag
 from nltk.tokenize import word_tokenize
@@ -10,6 +9,13 @@ from typing import List
 import pandas as pd
 import re
 from textblob import TextBlob
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.pre_tokenizers import PreTokenizer
+
+from typing import Optional
 
 # Télécharger les stopwords et le lemmatizer de WordNet Si besoin
 nltk.download("punkt")
@@ -29,22 +35,18 @@ def lemmatize_normalize_text(tokens):
     # Create an output list using list comprehension for optimal performance
     lemmatized_tokens = [
         (
-            token
-            if token.isupper()  # Keep uppercase tokens as they are (proper nouns)
+            lemmatizer.lemmatize(token.lower(), pos="v")
+            if pos.startswith("V")
             else (
-                lemmatizer.lemmatize(token.lower(), pos="v")
-                if pos.startswith("V")
+                lemmatizer.lemmatize(token.lower(), pos="a")
+                if pos.startswith("J")
                 else (
-                    lemmatizer.lemmatize(token.lower(), pos="a")
-                    if pos.startswith("J")
+                    lemmatizer.lemmatize(token.lower(), pos="r")
+                    if pos.startswith("R")
                     else (
-                        lemmatizer.lemmatize(token.lower(), pos="r")
-                        if pos.startswith("R")
-                        else (
-                            lemmatizer.lemmatize(token.lower(), pos="n")
-                            if pos.startswith("N")
-                            else lemmatizer.lemmatize(token.lower())
-                        )
+                        lemmatizer.lemmatize(token.lower(), pos="n")
+                        if pos.startswith("N")
+                        else lemmatizer.lemmatize(token.lower())
                     )
                 )
             )
@@ -131,7 +133,9 @@ def replace_emojis(text: str) -> str:
     return cleaned_text
 
 
-def remove_special_characters(text: str, remove_punctuations=False) -> str:
+def remove_special_characters(
+    text: str, remove_punctuations=False, remove_duplications=False, space_after_punctuations=True
+) -> str:
     """
     Supprime les caractères spéciaux, les adresses e-mail, les URLs, etc. du texte.
 
@@ -155,7 +159,9 @@ def remove_special_characters(text: str, remove_punctuations=False) -> str:
     )
     # Suppression des mots contenant '@'
     # cleaned_text = re.sub(r'\b\w*@\w*\b', '', cleaned_text)
-    cleaned_text = re.sub(r"wikipedia[\w%\'\.]*", " ", cleaned_text, flags=re.IGNORECASE)
+    cleaned_text = re.sub(
+        r"wikipedia[\w%\'\.]*", " ", cleaned_text, flags=re.IGNORECASE
+    )
     # Remplacement des motifs ressemblant à un pénis par le mot 'penis'
     cleaned_text = re.sub(r"\b8=+[=d]+", "penis", cleaned_text)
 
@@ -164,8 +170,17 @@ def remove_special_characters(text: str, remove_punctuations=False) -> str:
 
     if remove_punctuations:
         # Suppresion des ponctuations
-        cleaned_text = re.sub(r"[^\w\s]", " ", cleaned_text)
-    cleaned_text = re.sub(r'\s+', ' ', text)
+        if space_after_punctuations:
+            cleaned_text = re.sub(r"[^\w\s]", " ", cleaned_text)
+        else:
+            cleaned_text = re.sub(r"[^\w\s]", "", cleaned_text)
+
+    cleaned_text = re.sub(r"[^\x00-\x7F]+", " ", cleaned_text)  # Keep ascii
+    if remove_duplications:
+        cleaned_text = re.sub(
+            r"(.)\1{2,}", r"\1", cleaned_text
+        )  # Replace youuuuuuuu by you
+    cleaned_text = re.sub(r"\s+", " ", cleaned_text)  # Delete all big spaces
     return cleaned_text
 
 
@@ -177,7 +192,10 @@ def preprocess_text(
     remove_stopwords: bool = True,
     is_lemmatization: bool = True,
     remove_punctuations=True,
+    space_after_punctuations = True,
+    remove_duplications=True,
     tokenize=word_tokenize,
+    is_pretokenizer = False
 ) -> str:
     """
     Prétraitement complet du texte : correction de l'orthographe, remplacement des emojis,
@@ -192,23 +210,30 @@ def preprocess_text(
     if is_replace_emojis:
         text = replace_emojis(text)
     if is_remove_special_characters:
-        text = remove_special_characters(text, remove_punctuations)
+        text = remove_special_characters(text, remove_punctuations, remove_duplications,space_after_punctuations)
     if is_lemmatization:
-        tokens = word_tokenize(text)
-        tokens = lemmatize_normalize_text(tokens)
+        tokens = word_tokenize(text) if not is_pretokenizer else tokenize(text)
         if is_lowercase:
             tokens = [word.lower() for word in tokens]
+        tokens = lemmatize_normalize_text(tokens)
         if remove_stopwords:
             tokens = [word for word in tokens if word not in stop_words]
-        return " ".join(tokenize(" ".join(tokens))) if tokenize != word_tokenize else " ".join(tokens)
+        return (
+            " ".join(tokenize(" ".join(tokens)))
+            if tokenize != word_tokenize
+            else " ".join(tokens)
+        )
     elif is_lowercase:
         text = text.lower()
         if remove_stopwords:
-            tokens = word_tokenize(text)
+            tokens = word_tokenize(text) if not is_pretokenizer else tokenize(text)
             tokens = [word for word in tokens if word not in stop_words]
-            return " ".join(tokenize(" ".join(tokens))) if tokenize != word_tokenize else " ".join(tokens)
+            return (
+                " ".join(tokenize(" ".join(tokens)))
+                if tokenize != word_tokenize
+                else " ".join(tokens)
+            )
     return " ".join(tokenize(text))
-
 
 
 def gpt_tokenize(
@@ -236,7 +261,7 @@ def gpt_tokenize(
         normalized_text = viet_strings
     enc = tiktoken.encoding_for_model("gpt-4")
     tokens = enc.encode(normalized_text)
-    decoded_tokens = enc.decode(tokens)
+    # decoded_tokens = enc.decode(tokens)
     # decoded_words = decoded_tokens.split()
     return [str(token) for token in tokens]
 
@@ -274,11 +299,39 @@ def byte_pair_tokenize(
         tokenizer.train(normalized_text, vocab_size=vocab_size)
         tokenizer.save("byte_pair_tokenizer")
 
-    viet_tokens = tokenizer.encode(normalized_text)
-    decoded_tokens = tokenizer.decode(viet_tokens)
-    decoded_words = decoded_tokens.split()
+    tokens = tokenizer.encode(normalized_text)
+    decoded_tokens = tokenizer.decode(tokens)
 
-    return decoded_words
+    return [str(token) for token in tokens]
+
+
+
+def byte_pair_tokenize_transformer(
+    sentence,tokenizer=None, comment_text_train: Optional[pd.DataFrame] = None, vocab_size: int = 65536, save_path = "data/trained_tokenizer.json" 
+):
+    # Sample text data (in practice, use a large corpus)
+
+    # Initialize a BPE tokenizer
+    if tokenizer is None:
+        tokenizer = Tokenizer(BPE())
+
+        # Set the normalizer and pre-tokenizer
+        tokenizer.pre_tokenizer = Whitespace() # type: ignore
+
+        # Initialize the BPE trainer with a specified vocabulary size
+        vocab_size = vocab_size  # Set your desired vocabulary size
+        trainer = BpeTrainer(vocab_size=vocab_size) # type: ignore
+
+        # Train the tokenizer on your text data
+        tokenizer.train_from_iterator(comment_text_train, trainer=trainer)
+
+        # Save the trained tokenizer to a file
+        tokenizer.save(save_path)
+
+    # Load the trained tokenizer (for future use
+    # Tokenize a new sentence using the trained tokenizer
+    tokens = tokenizer.encode(sentence) # type: ignore
+    return tokens.tokens
 
 
 def regex_tokenize(
@@ -322,40 +375,60 @@ def regex_tokenize(
 
 
 if __name__ == "__main__":
-    import preprocessing
+    print(
+        byte_pair_tokenize(
+            """What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
-    (df_train, df_val, df_test) = preprocessing.load_dataframes()
+Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
 
-    # Texte initial
-    # original_text = df_test["comment_text"][0] #met ta phrase pour testt
-    original_text = "Thankz yuu for understanding. I thinkk very highly of you and wouldd not revert without this discussion. :) 8=="
-    print("Texte initial:")
-    print(original_text)
-    print()
 
-    # Texte après correction de l'orthographe
-    corrected_text = correct_spelling(original_text)
-    print("Texte après correction de l'orthographe:")
-    print(corrected_text)
-    print()
+Where does it come from?
+Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
 
-    # Texte après remplacement des emojis
-    emoji_replaced_text = replace_emojis(corrected_text)
-    print("Texte après remplacement des emojis:")
-    print(emoji_replaced_text)
-    print()
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
 
-    # Texte après suppression des caractères spéciaux
-    special_chars_removed_text = remove_special_characters(emoji_replaced_text)
-    print("Texte après suppression des caractères spéciaux:")
-    print(special_chars_removed_text)
-    print()
+Where can I get some?
+There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate L""",
+            normalize=True,
+            vocab_size=65536,
+        )
+    )
+    # import preprocessing
 
-    # Texte après normalisation
-    normalized_text = preprocess_text(original_text)
-    print("Texte après normalisation complète:")
-    print(normalized_text)
-    print()
+    # (df_train, df_val, df_test) = preprocessing.load_dataframes()
+
+    # # Texte initial
+    # # original_text = df_test["comment_text"][0] #met ta phrase pour testt
+    # original_text = "Thankz yuu for understanding. I thinkk very highly of you and wouldd not revert without this discussion. :) 8=="
+    # print("Texte initial:")
+    # print(original_text)
+    # print()
+
+    # # Texte après correction de l'orthographe
+    # corrected_text = correct_spelling(original_text)
+    # print("Texte après correction de l'orthographe:")
+    # print(corrected_text)
+    # print()
+
+    # # Texte après remplacement des emojis
+    # emoji_replaced_text = replace_emojis(corrected_text)
+    # print("Texte après remplacement des emojis:")
+    # print(emoji_replaced_text)
+    # print()
+
+    # # Texte après suppression des caractères spéciaux
+    # special_chars_removed_text = remove_special_characters(emoji_replaced_text)
+    # print("Texte après suppression des caractères spéciaux:")
+    # print(special_chars_removed_text)
+    # print()
+
+    # # Texte après normalisation
+    # normalized_text = preprocess_text(original_text)
+    # print("Texte après normalisation complète:")
+    # print(normalized_text)
+    # print()
 
     # # Test avec GPT Tokenizer sans normalisation
     # tokens = gpt_tokenize(df_test)
